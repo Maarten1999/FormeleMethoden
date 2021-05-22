74347b7a88a9b8f22413f14c400856a87ca37a6b
Dion <dionvanderlinden@live.nl>
2021-05-22 16:42:54 +0200

    Regex accept + test functies

--- FormeleMethodenCS/Program.cs
+++ FormeleMethodenCS/Program.cs
@@ -10,6 +10,20 @@ namespace FormeleMethodenCS
     {
         static void Main(string[] args)
         {
+
+            TestRegex();
+            Console.ReadLine();
+
+            //var x = dfa.GetToStates("q4", 'a');
+            //foreach (var i in x)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
+        }
+
+        static void TestDFA()
+        {
             char[] alphabet = { 'a', 'b' };
 
             Automata<string> dfa = new DFA<string>(alphabet);
@@ -39,7 +53,7 @@ namespace FormeleMethodenCS
 
             Graphiz<string> graphiz = new Graphiz<string>(dfa);
             graphiz.PrintGraph();
-            
+
             List<string> words = dfa.GetLanguage(6, true).ToList();//dfa.GetLanguage(2);
             Console.WriteLine($"Number of combinations: {words.Count()}");
             words.ForEach(w => Console.WriteLine(w));
@@ -53,15 +67,56 @@ namespace FormeleMethodenCS
                 Console.WriteLine($"String geaccepteerd: {success}");
                 Console.WriteLine("Druk op esc om te stoppen.");
             } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+        static void TestRegex()
+        {
+            RegExp a = new RegExp("a");
+            RegExp b = new RegExp("b");
 
+            // Expr1: "baa"
+            RegExp expr1 = new RegExp("baa");
+            // Expr2: "bb"
+            RegExp expr2 = new RegExp("bb");
+            // Expr3: "baa | bb"
+            RegExp expr3 = expr1.Or(expr2);
 
+            // All: " (a|b) *"
+            RegExp all = (a.Or(b)).Star();
+            // Expr4: " (baa | bb)+"
+            RegExp expr4 = expr3.Plus();
+            // Expr5: "(baa | bb)+ (a|b)"
+            RegExp expr5 = expr4.Dot(all);
 
-            //var x = dfa.GetToStates("q4", 'a');
-            //foreach (var i in x)
-            //{
-            //    Console.WriteLine(i);
-            //}
-            //Console.ReadLine();
+            //Console.WriteLine("Taal van (baa):");
+            //expr1.GetLanguage(5).ToList().ForEach(s => Console.WriteLine(s));
+            //Console.WriteLine("");
+            //Console.WriteLine("Taal van (bb):");
+            //expr2.GetLanguage(5).ToList().ForEach(s => Console.WriteLine(s));
+            //Console.WriteLine("");
+
+            Console.WriteLine("Taal van (baa | bb):");
+            expr3.GetLanguage(5).ToList().ForEach(s => Console.WriteLine(s));
+            Console.WriteLine("");
+
+            //Console.WriteLine("Taal van (a|b)*:");
+            //all.GetLanguage(5).ToList().ForEach(s => Console.WriteLine(s));
+            //Console.WriteLine("");
+
+            //Console.WriteLine("Taal van (baa | bb)+:");
+            //expr4.GetLanguage(5).ToList().ForEach(s => Console.WriteLine(s));
+            //Console.WriteLine("");
+
+            //Console.WriteLine("Taal van (baa | bb) + (a|b) *:");
+            //expr5.GetLanguage(6).ToList().ForEach(s => Console.WriteLine(s));
+            //Console.WriteLine("");
+            string str;
+            do
+            {
+                Console.Write("Input: ");
+                str = Console.ReadLine();
+                Console.WriteLine($"Accepts {expr3.ToString()} = {expr3.Accept(str)}");
+            } while (str != "exit");
+            
         }
     }
 }
