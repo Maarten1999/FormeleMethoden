0b0ad36667af93270c2b7f7d1af047d41c7f9584
Dion <dionvanderlinden@live.nl>
2021-06-06 18:58:01 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- FormeleMethodenCS/DFAGenerator.cs
+++ FormeleMethodenCS/DFAGenerator.cs
@@ -14,20 +14,20 @@ namespace FormeleMethodenCS
             DFA<string> dfa = new DFA<string>(alphabet);
 
             string startState = "q0";
-            string errorState = string.Format("q{0}", str.Length+1);
+            string errorState = string.Format("q{0}", str.Length + 1);
             string finalState = string.Format("q{0}", str.Length);
             for (int i = 0; i < str.Length; i++)
             {
                 char c = str[i];
-                dfa.AddTransition(new Transition<string>("q"+i, c, string.Format("q{0}", (i+1))));
+                dfa.AddTransition(new Transition<string>("q" + i, c, string.Format("q{0}", (i + 1))));
                 for (int j = 0; j < alphabet.Length; j++)
                 {
                     char otherChar = alphabet[j];
                     if (otherChar != c)
                     {
-                        dfa.AddTransition(new Transition<string>("q"+i, otherChar, errorState));
+                        dfa.AddTransition(new Transition<string>("q" + i, otherChar, errorState));
                     }
-                }              
+                }
             }
             // Fuik states maken voor final en error
             foreach (char c in alphabet)
